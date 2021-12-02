d33f880c833e6810f0d965f5688eb9a8231fbf7d
Wisesa <hi@wisesa.dev>
2021-12-02 21:46:56 +0700

    chore: add pokeapi type-def

---
+++ src/types/pokeapi.d.ts
@@ -0,0 +1,114 @@
+//Generated from https://graphql-pokeapi.graphcdn.app/ schema
+
+namespace Pokemon {
+  type BaseList = {
+    count: number;
+    next: string;
+    previous: string;
+    results: BaseName[];
+    status: boolean;
+    message: string;
+  };
+
+  type Ability = {
+    ability: BaseName;
+    is_hidden: boolean;
+    slot: number;
+  };
+
+  type GameIndex = {
+    game_index: number;
+    version: BaseName;
+  };
+
+  type VersionDetail = {
+    rarity: number;
+    version: BaseName;
+  };
+
+  type HeldItem = {
+    item: BaseName;
+    version_details: VersionDetail[];
+  };
+
+  type VersionGroupDetail = {
+    level_learned_at: number;
+    move_learn_method: BaseName;
+    version_group: BaseName;
+  };
+
+  type Move = {
+    move: BaseName;
+    version_group_details: VersionGroupDetail[];
+  };
+
+  type Sprite = {
+    back_default: string;
+    back_female: string;
+    back_shiny: string;
+    back_shiny_female: string;
+    front_default: string;
+    front_female: string;
+    front_shiny: string;
+    front_shiny_female: string;
+  };
+
+  type Stat = {
+    base_stat: number;
+    effort: number;
+    stat: BaseName;
+  };
+
+  type Type = {
+    slot: number;
+    type: BaseName;
+  };
+
+  type BaseName = {
+    id: number;
+    url: string;
+    name: string;
+  };
+
+  type Pokemon = {
+    abilities: Ability[];
+    base_experience: number;
+    forms: BaseName[];
+    game_indices: GameIndex[];
+    height: number;
+    held_items: HeldItem[];
+    id: number;
+    is_default: boolean;
+    location_area_encounters: string;
+    moves: Move[];
+    name: string;
+    order: number;
+    species: BaseName;
+    sprites: Sprite;
+    stats: Stat[];
+    types: Type[];
+    weight: number;
+    status: boolean;
+    message: string;
+  };
+
+  type PokemonItem = {
+    id: number;
+    url: string;
+    name: string;
+    image: string;
+    artwork: string;
+    dreamworld: string;
+  };
+
+  type PokemonList = {
+    count: number;
+    next: string;
+    previous: string;
+    nextOffset: number;
+    prevOffset: number;
+    results: PokemonItem[];
+    status: boolean;
+    message: string;
+  };
+}
