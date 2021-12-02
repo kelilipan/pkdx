7211407bbaa26d2bcf396f73562c02dfd3c86457
Wisesa <hi@wisesa.dev>
2021-12-02 14:08:11 +0700

    chore: add prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <button css={{ color: "red" }}>Hello world!</button>
       </header>
     </div>
   );
