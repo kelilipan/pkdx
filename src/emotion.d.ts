cddce283226bfd691d8473053cdc4a080b2d8ae1
Wisesa <hi@wisesa.dev>
2021-12-03 18:49:25 +0700

    fix: theme typedef

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -3,27 +3,7 @@ import "@emotion/react";
 declare module "@emotion/react" {
   export interface Theme {
     color: {
-      type: {
-        normal: string;
-        fire: string;
-        water: string;
-        grass: string;
-        electric: string;
-        ice: string;
-        fighting: string;
-        poison: string;
-        ground: string;
-        flying: string;
-        psychic: string;
-        bug: string;
-        rock: string;
-        ghost: string;
-        dark: string;
-        dragon: string;
-        steel: string;
-        fairy: string;
-        unkown: string;
-      };
+      type: Record<string, string>;
     };
   }
 }
