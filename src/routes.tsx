2184fb0d3c8911ce024a24c001976f6397413a0b
Wisesa <hi@wisesa.dev>
2021-12-05 01:17:54 +0700

    feat: reset scroll position

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,8 +3,13 @@ import Home from "pages/home";
 import PokemonDetail from "pages/pokemon-detail";
 import MyPokemon from "pages/my-pokemon";
 import NotFound from "pages/404";
+import { useEffect } from "react";
+import { useLocation } from "react-router";
 
 const Routes = () => {
+  const location = useLocation();
+  useEffect(() => window.scroll(0, 0), [location]);
+
   return (
     <ReactRoutes>
       <Route path="/" element={<Home />} />
