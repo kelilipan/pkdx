e16bcd8fa089abdb29d110cfc710b5eb5f3d3620
Wisesa <hi@wisesa.dev>
2021-12-05 15:58:18 +0700

    style: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,46 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
 import Container from "components/layout/container";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <Container>404 not found</Container>;
+  const location = useLocation();
+  const container = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    h1 {
+      font-weight: bold;
+      font-size: 2rem;
+    }
+    p {
+      margin-top: 1em;
+      strong {
+        font-weight: bold;
+      }
+    }
+  `;
+  return (
+    <Container css={container}>
+      <h1>Not found</h1>
+      <p>
+        Page <strong>{location.pathname}</strong> is not available.
+      </p>
+      <Link
+        to="/"
+        css={{
+          marginTop: "0.5em",
+          textDecoration: "underline",
+        }}
+      >
+        Back to list
+      </Link>
+    </Container>
+  );
 };
 
 export default NotFound;
