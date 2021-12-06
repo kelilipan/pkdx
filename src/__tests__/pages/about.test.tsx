606890a385781f1a15fa830490ddbd01ffaf2b02
Wisesa <hi@wisesa.dev>
2021-12-06 22:56:53 +0700

    test: about page

--- src/__tests__/pages/about.test.tsx
+++ src/__tests__/pages/about.test.tsx
@@ -16,9 +16,11 @@ describe("About page", () => {
     const description = screen.getByText(
       /a simple pokedex web app built with react\.js & typescript made by \./i
     );
+    const myLink = screen.getByRole("link", { name: /wisesa/i });
     const ossText = screen.getByText(
-      /this app is open source, you can see the source code \./i
+      /this app is open source, you can see the \./i
     );
+    const sourceLink = screen.getByRole("link", { name: /source code here/i });
     const pokemonCopyright = screen.getByText(
       /pokémon © 2002\-2021 pokémon\. © 1995\-2021 nintendo\/creatures inc\.\/game freak inc\. tm, ® and pokémon character names are trademarks of nintendo\./i
     );
@@ -26,7 +28,9 @@ describe("About page", () => {
     expect(image).toBeVisible();
     expect(pkdx).toBeVisible();
     expect(description).toBeVisible();
+    expect(myLink).toBeVisible();
     expect(ossText).toBeVisible();
+    expect(sourceLink).toBeVisible();
     expect(pokemonCopyright).toBeVisible();
   });
 });
